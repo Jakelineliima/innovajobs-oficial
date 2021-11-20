8c1f0ecaca7853b6ddfa6ece9ee84fc7b0bf405a
Jakeline Liima <jakeline.liima555@gmail.com>
2021-11-20 03:42:11 -0300

    update indexphp
    Correção do index

--- webroot/index.php
+++ webroot/index.php
@@ -20,18 +20,18 @@
 require dirname(__DIR__) . '/config/requirements.php';
 
 // For built-in server
-// if (PHP_SAPI === 'cli-server') {
-//     $_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
+if (PHP_SAPI === 'cli-server') {
+    $_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
 
-//     $url = parse_url(urldecode($_SERVER['REQUEST_URI']));
-//     $file = __DIR__ . $url['path'];
-//     if (strpos($url['path'], '..') === false && strpos($url['path'], '.') !== false && is_file($file)) {
-//         return false;
-//     }
-// }
-if (!defined('CAKE_CORE_INCLUDE_PATH')) {
-    define('CAKE_CORE_INCLUDE_PATH', ROOT);
+    $url = parse_url(urldecode($_SERVER['REQUEST_URI']));
+    $file = __DIR__ . $url['path'];
+    if (strpos($url['path'], '..') === false && strpos($url['path'], '.') !== false && is_file($file)) {
+        return false;
+    }
 }
+// if (!defined('CAKE_CORE_INCLUDE_PATH')) {
+//     define('CAKE_CORE_INCLUDE_PATH', ROOT);
+// }
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 use App\Application;
